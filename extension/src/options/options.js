17bc260a89a7c1a16e87d298defef1498093d0b9
Varun Srivastava <varun.neal@berkeley.edu>
2023-09-03 14:19:28 -0700

    solved some warnings

--- extension/src/options/options.js
+++ extension/src/options/options.js
@@ -1,20 +1,12 @@
 
-console.log("Script loaded");
-let loadCount = 0;
-
 document.addEventListener('DOMContentLoaded', function() {
-    loadCount++;
-    console.log("DOMContentLoaded triggered", loadCount);
-    // Load saved settings when the page is loaded
     loadSettings();
 
-    // Save settings when the "Save Settings" button is clicked
     const b = document.getElementById('saveButton')
     if (b) {
         b.addEventListener('click', saveSettings);
     }
 
-    // Restore default settings when the "Restore Defaults" button is clicked
     const r = document.getElementById('restoreButton')
     if (r) {
         r.addEventListener('click', restoreDefaults);
@@ -39,8 +31,7 @@ function restoreDefaults() {
     document.getElementById('modelSelector').value = 'Supabase/gte-small'; // Default model
     document.getElementById('minCharsInput').value = 50; // Default number
 
-    // Optional: Automatically save defaults back to storage
-    saveSettings(false);  // Don't show an alert here
+    saveSettings(false);
 }
 
 
