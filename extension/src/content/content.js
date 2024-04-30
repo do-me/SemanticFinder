7277ab5f27b39d77e39b4a2a372bcb95cef6407a
catmaniscatlord <catmanisacatlord@gmail.com>
2024-04-29 18:23:13 -0600

    less hacky pdf detection

--- extension/src/content/content.js
+++ extension/src/content/content.js
@@ -10,98 +10,98 @@ async function fetchAndExtractPDFText(url) {
 
     const pdf = await getDocument(url).promise;
 
-    let totalPages = pdf.numPages;
-    let texts = [];
+  let totalPages = pdf.numPages;
+  let texts = [];
 
-    for (let i = 1; i <= totalPages; i++) {
-        // console.log("page ", i);
-        const page = await pdf.getPage(i);
-        const textContent = await page.getTextContent();
-        const pageText = textContent.items.map(item => item.str).join(' ');
-        texts.push(pageText);
-    }
+  for (let i = 1; i <= totalPages; i++) {
+    // console.log("page ", i);
+    const page = await pdf.getPage(i);
+    const textContent = await page.getTextContent();
+    const pageText = textContent.items.map(item => item.str).join(' ');
+    texts.push(pageText);
+  }
 
-    return texts.join(' ');
+  return texts.join(' ');
 }
 
 function getValueFromStorage(key, defaultValue) {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(key, function(result) {
-            if (chrome.runtime.lastError) {
-                reject(new Error(chrome.runtime.lastError));
-            } else {
-                resolve(result[key] || defaultValue);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(key, function(result) {
+      if (chrome.runtime.lastError) {
+        reject(new Error(chrome.runtime.lastError));
+      } else {
+        resolve(result[key] || defaultValue);
+      }
     });
+  });
 }
 
 async function fetchNumChars() {
-    try {
-        const defaultNumChars = 50; // You can set this to your desired default value
-        const storedNumChars = await getValueFromStorage('num_chars', defaultNumChars);
-        return storedNumChars;
-    } catch (error) {
-        console.error('Error fetching num_chars:', error);
-        return null;
-    }
+  try {
+    const defaultNumChars = 50; // You can set this to your desired default value
+    const storedNumChars = await getValueFromStorage('num_chars', defaultNumChars);
+    return storedNumChars;
+  } catch (error) {
+    console.error('Error fetching num_chars:', error);
+    return null;
+  }
 }
 
 chrome.runtime.onMessage.addListener(async function(request, sender) {
-    try {
-        let currentURL = window.location.href;
-        if (request.type === "getText") {
-            const numChars = await fetchNumChars();
-            let texts = [];
-            // hacky support for pdf
-            if (currentURL.includes('pdf')) {
-                let textContent = await fetchAndExtractPDFText(currentURL);
-                texts = splitReadableContent(textContent, numChars);
+  try {
+    let currentURL = window.location.href;
+    if (request.type === "getText") {
+      const numChars = await fetchNumChars();
+      let texts = [];
 
-            } else {
-                let concatenatedContent = "";
+      if (request.contentType == "application/pdf") {
+        let textContent = await fetchAndExtractPDFText(currentURL);
+        texts = splitReadableContent(textContent, numChars);
 
-                const iframes = document.querySelectorAll('iframe');
-                console.dir(iframes);
+      } else {
+        let concatenatedContent = "";
 
-                iframes.forEach(function(iframe) {
-                    try {
-                        const iframeDocument = iframe.contentDocument;
+        const iframes = document.querySelectorAll('iframe');
+        console.dir(iframes);
 
-                        if (iframeDocument) {
+        iframes.forEach(function(iframe) {
+          try {
+            const iframeDocument = iframe.contentDocument;
 
+            if (iframeDocument) {
+              
                             let { textContent } = new Readability(iframeDocument.cloneNode(true)).parse();
-                            prettyLog("Iframe text content:", textContent, "orange");
-                            concatenatedContent += textContent;
-                        }
-                    } catch (error) {
-                        prettyLog("Skipped an iframe due to permissions issue:", error, "red");
-                    }
-                });
-
-                const documentClone = document.cloneNode(true);
-                let { textContent } = new Readability(documentClone).parse();
-                concatenatedContent += textContent;
-                // prettyLog("Main document text content:", textContent);
-
-                texts = splitReadableContent(concatenatedContent, numChars);
-
+              prettyLog("Iframe text content:", textContent, "orange");
+              concatenatedContent += textContent;
             }
-            chrome.runtime.sendMessage({type: "tabUpdated", text: texts, currentURL});
-        } else if (request.type === 'highlightAndScroll') {
-            // if (currentURL.endsWith('.pdf')) { return; }
-            if (!highlightAndScrollToText(request.text)) {
-                chrome.runtime.sendMessage({type: "error", reason: "Cannot find and highlight selection."})
+          } catch (error) {
+            prettyLog("Skipped an iframe due to permissions issue:", error, "red");
+          }
+        });
+
+        const documentClone = document.cloneNode(true);
+        let { textContent } = new Readability(documentClone).parse();
+        concatenatedContent += textContent;
+        // prettyLog("Main document text content:", textContent);
+
+        texts = splitReadableContent(concatenatedContent, numChars);
+      
             }
-        }
-    } catch (error) {
-        prettyLog("ERROR", error.message, "red", "red");
-        if (error.message.includes('net::ERR_BLOCKED_BY_CLIENT')) {
-            chrome.runtime.sendMessage({type: "error", reason: "ERR_BLOCKED_BY_CLIENT"});
-        } else {
-            chrome.runtime.sendMessage({type: "error", reason: error.message});
-        }
+      chrome.runtime.sendMessage({type: "tabUpdated", text: texts, currentURL});
+    } else if (request.type === 'highlightAndScroll') {
+      // if (currentURL.endsWith('.pdf')) { return; }
+      if (!highlightAndScrollToText(request.text)) {
+        chrome.runtime.sendMessage({type: "error", reason: "Cannot find and highlight selection."})
+      }
     }
+  } catch (error) {
+    prettyLog("ERROR", error.message, "red", "red");
+    if (error.message.includes('net::ERR_BLOCKED_BY_CLIENT')) {
+      chrome.runtime.sendMessage({type: "error", reason: "ERR_BLOCKED_BY_CLIENT"});
+    } else {
+      chrome.runtime.sendMessage({type: "error", reason: error.message});
+    }
+  }
 });
 
 
@@ -109,46 +109,46 @@ let currText;
 let instance = new Mark(document.querySelector("body"));
 
 function highlightAndScrollToText(text, depth= 3) {
-    if (depth === 0) {
-        return false;
+  if (depth === 0) {
+    return false;
+  }
+  // If there's a previous highlighted text, unmark it
+  if (currText) {
+    instance.unmark({"element": "span", "className": "SemanticFinder-highlight"});
+  }
+
+  currText = text;
+
+  let textFound = false;
+
+  instance.mark(text, {
+    "element": "span",
+    "separateWordSearch": false,
+    "className": "SemanticFinder-highlight",
+    "acrossElements": true,
+    "wildcards": "enabled",
+    "iframes": true,
+    "each": function (node) {
+      // Scroll to the first instance of it
+      node.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+      });
+      textFound = true;
     }
-    // If there's a previous highlighted text, unmark it
-    if (currText) {
-        instance.unmark({"element": "span", "className": "SemanticFinder-highlight"});
+  });
+
+
+  // can use "noMatch" in markjs instead
+  if (!textFound) {
+    let segments = text.split('\n');
+    let longestSegment = segments.sort((a, b) => b.length - a.length)[0];
+    if (longestSegment) {
+      return highlightAndScrollToText(longestSegment, depth - 1);
     }
-
-    currText = text;
-
-    let textFound = false;
-
-    instance.mark(text, {
-        "element": "span",
-        "separateWordSearch": false,
-        "className": "SemanticFinder-highlight",
-        "acrossElements": true,
-        "wildcards": "enabled",
-        "iframes": true,
-        "each": function (node) {
-            // Scroll to the first instance of it
-            node.scrollIntoView({
-                behavior: "smooth",
-                block: "center"
-            });
-            textFound = true;
-        }
-    });
-
-
-    // can use "noMatch" in markjs instead
-    if (!textFound) {
-        let segments = text.split('\n');
-        let longestSegment = segments.sort((a, b) => b.length - a.length)[0];
-        if (longestSegment) {
-            return highlightAndScrollToText(longestSegment, depth - 1);
-        }
-    } else {
-        return true;
-    }
+  } else {
+    return true;
+  }
 }
 
 
--- extension/src/popup/popup.vue
+++ extension/src/popup/popup.vue
@@ -1,42 +1,42 @@
 <template>
-    <div id="app" :class="popupClass">
+  <div id="app" :class="popupClass">
 
-        <div v-if="!isModelLoaded" class="progress-container">
-            <!--todo: make this its own vue component-->
-            <div class="progress-background">
-                <div class="progress-bar" :style="{ width: progressValue + `%`}"></div>
-            </div>
-            <div class="loading-text">Loading model</div>
-        </div>
-
-        <div v-else>
-            <div v-if="error" class="error">
-                <span class="dismiss" @click="dismissError">&#x2612;</span>
-                ERROR: {{ error }}
-            </div>
-            <AnimatedInput ref="input"></AnimatedInput>
-
-            <!-- Display results and progress only if popupClass is 'popup-expanded' -->
-            <div v-if="popupClass === 'popup-expanded'">
-                <div class="results-container">
-                    <ResultItem
-                            v-for="(result, index) in results"
-                            :key="index"
-                            :result="result.text"
-                            :score="result.sim"
-                            @click="handleResultClick(result)"
-                    />
-                </div>
-
-                <div class="progress-container search-progress">
-                    <div class="progress-bar" :style="{ width: searchProgress + `%` }"></div>
-                </div>
-
-            </div>
-        </div>
-
-
+    <div v-if="!isModelLoaded" class="progress-container">
+      <!--todo: make this its own vue component-->
+      <div class="progress-background">
+        <div class="progress-bar" :style="{ width: progressValue + `%`}"></div>
+      </div>
+      <div class="loading-text">Loading model</div>
     </div>
+
+    <div v-else>
+      <div v-if="error" class="error">
+        <span class="dismiss" @click="dismissError">&#x2612;</span>
+        ERROR: {{ error }}
+      </div>
+      <AnimatedInput ref="input"></AnimatedInput>
+
+      <!-- Display results and progress only if popupClass is 'popup-expanded' -->
+      <div v-if="popupClass === 'popup-expanded'">
+        <div class="results-container">
+          <ResultItem
+            v-for="(result, index) in results"
+            :key="index"
+            :result="result.text"
+            :score="result.sim"
+            @click="handleResultClick(result)"
+          />
+        </div>
+
+        <div class="progress-container search-progress">
+          <div class="progress-bar" :style="{ width: searchProgress + `%` }"></div>
+</div>
+
+            </div>
+        </div>
+      
+
+  </div>
 </template>
 
 
@@ -47,78 +47,88 @@ import AnimatedInput from './AnimatedInput.vue'
 import {prettyLog} from "../utils/utils.js";
 
 export default {
-    components: {
-        AnimatedInput,
-        ResultItem,
+  components: {
+    AnimatedInput,
+    ResultItem,
+  },
+  data() {
+    return {
+      results: [],
+      progressValue: 0,
+      searchProgress: 0,
+      isModelLoaded: false,
+      error: undefined,
+    };
+  },
+  computed: {
+    popupClass() {
+      return this.results.length > 0 ? 'popup-expanded' : 'popup-default';
     },
-    data() {
-        return {
-            results: [],
-            progressValue: 0,
-            searchProgress: 0,
-            isModelLoaded: false,
-            error: undefined,
-        };
-    },
-    computed: {
-        popupClass() {
-            return this.results.length > 0 ? 'popup-expanded' : 'popup-default';
-        },
-    },
-    watch: {},
-    methods: {
-        async handleMessage(request, sender, sendResponse) {
-            // console.dir(request);
-            switch (request.type) {
-                case "results":
-                    if ('text' in request) {
-                        this.results = request.text;
-                    }
-                    this.searchProgress = request.progress;
+  },
+  watch: {},
+  methods: {
+    async handleMessage(request, sender, sendResponse) {
+      // console.dir(request);
+      switch (request.type) {
+        case "results":
+          if ('text' in request) {
+            this.results = request.text;
+          }
+          this.searchProgress = request.progress;
 
-                    break;
-                case "download":
-                    if (request.data.file && request.data.file !== "onnx/model_quantized.onnx") {
-                        break;
-                    }
-                    if (request.data.status === 'progress') {
-                        this.progressValue = request.data.progress.toFixed(2);
-                    } else if (request.data.status === 'complete') {
-                        this.progressValue = 100;
-                        this.isModelLoaded = true;
-                    }
-                    break;
-                case "error":
-                    this.error = request.reason;
-            }
-        },
-
-        // todo: move to result.vue
-        handleResultClick(result) {
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {type: 'highlightAndScroll', text: result.text});
-            });
-        },
-        dismissError() {
-            this.error = null;
-        },
+          break;
+        case "download":
+          if (request.data.file && request.data.file !== "onnx/model_quantized.onnx") {
+            break;
+          }
+          if (request.data.status === 'progress') {
+            this.progressValue = request.data.progress.toFixed(2);
+          } else if (request.data.status === 'complete') {
+            this.progressValue = 100;
+            this.isModelLoaded = true;
+          }
+          break;
+        case "error":
+          this.error = request.reason;
+      }
     },
 
-    async mounted() {
-        chrome.runtime.onMessage.addListener(this.handleMessage);
-        const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-
-        chrome.tabs.sendMessage(tab.id, {type: "getText"});
-        chrome.runtime.sendMessage({type: "load"});
-
-        this.results = [];
-
+    // todo: move to result.vue
+    handleResultClick(result) {
+      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: 'highlightAndScroll', text: result.text});
+      });
     },
-    beforeUnmount() {
-        this.results = [];
-        chrome.runtime.sendMessage({type: "pruneEmbeddings"});
-        chrome.runtime.onMessage.removeListener(this.handleMessage);
+    dismissError() {
+      this.error = null;
     },
+  },
+
+  async mounted() {
+    chrome.runtime.onMessage.addListener(this.handleMessage);
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    const [res] = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        return document.contentType;
+      },
+    });
+
+    chrome.tabs.sendMessage(tab.id, {
+      type: "getText",
+      contentType: res.result,
+    });
+    chrome.runtime.sendMessage({type: "load"});
+
+    this.results = [];
+
+  },
+  beforeUnmount() {
+    this.results = [];
+    chrome.runtime.sendMessage({type: "pruneEmbeddings"});
+    chrome.runtime.onMessage.removeListener(this.handleMessage);
+  },
 };
 </script>
 
@@ -127,44 +137,44 @@ export default {
 
 
 #app {
-    padding: 0;
-    margin: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 
 #app.popup-default {
-    width: 150px;
-    transition: width 0.5s ease;
+  width: 150px;
+  transition: width 0.5s ease;
 }
 
 #app.popup-expanded {
-    width: 300px;
-    transition: width 0.5s ease;
+  width: 300px;
+  transition: width 0.5s ease;
 }
 
 .progress-container {
-    position: relative;
+  position: relative;
 }
 
 
 .loading-text {
-    color: white;
-    font-size: 12px;
-    font-family: 'Space Mono', 'monospace';
-    font-weight: 700;
-    position: absolute;
-    z-index: 2;
-    white-space: nowrap;
-    overflow: hidden;
+  color: white;
+  font-size: 12px;
+  font-family: 'Space Mono', 'monospace';
+  font-weight: 700;
+  position: absolute;
+  z-index: 2;
+  white-space: nowrap;
+  overflow: hidden;
 
-    top: 50%;
-    transform: translateY(-50%);
-    left: 7.5px;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 7.5px;
 
-    text-shadow: -1px -1px 0 #000,
+  text-shadow: -1px -1px 0 #000,
     1px -1px 0 #000,
     -1px 1px 0 #000,
     1px 1px 0 #000;
@@ -172,65 +182,65 @@ export default {
 
 
 .progress-background {
-    width: 140px;
-    height: 30px;
-    background-color: black;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    overflow: hidden;
-    padding: 0 5px;
+  width: 140px;
+  height: 30px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  overflow: hidden;
+  padding: 0 5px;
 }
 
 
 .progress-bar {
-    height: 22px;
-    background-color: #525252;
-    clip-path: polygon(0 0, 75% 0, 100% 100%, 0 100%);
-    z-index: 2;
+  height: 22px;
+  background-color: #525252;
+  clip-path: polygon(0 0, 75% 0, 100% 100%, 0 100%);
+  z-index: 2;
 }
 
 
 .results-container {
-    max-height: 400px;
-    width: 290px;
-    overflow-y: auto;
-    padding: 10px;
+  max-height: 400px;
+  width: 290px;
+  overflow-y: auto;
+  padding: 10px;
 }
 
 .progress-container.search-progress >>> .progress-bar {
-    height: 12px;
-    background-color: #525252;
-    color: #525252;
-    clip-path: none;
+  height: 12px;
+  background-color: #525252;
+  color: #525252;
+  clip-path: none;
 }
 
 .progress-bar[style*="100%"] {
-    clip-path: none;
+  clip-path: none;
 }
 
 .error {
-    font-family: 'Space Mono', 'monospace';
-    background: red;
-    color: white;
-    z-index: 3;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    font-size: 11px;
-    line-height: 1;
-    padding: 15px 10px 10px 10px;  /* Adjust as needed */
+  font-family: 'Space Mono', 'monospace';
+  background: red;
+  color: white;
+  z-index: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  font-size: 11px;
+  line-height: 1;
+  padding: 15px 10px 10px 10px;  /* Adjust as needed */
 }
 
 
 .dismiss {
-    position: absolute;
-    top: 0px;
-    left: 8px;
-    cursor: pointer;
-    font-weight: bold;
-    font-size: 28px;
+  position: absolute;
+  top: 0px;
+  left: 8px;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 28px;
 }
 
 </style>
