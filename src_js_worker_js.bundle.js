bf823505ac7aa48fc69a01cc6ea1ce26898141ed
do-me <do-me@users.noreply.github.com>
2024-08-29 07:58:02 +0000

    deploy: 2c480e16ac5678c6ccc391e62ed8e9b0550d7875

--- src_js_worker_js.bundle.js
+++ src_js_worker_js.bundle.js
@@ -286,7 +286,7 @@ eval("/* (ignored) */\n\n//# sourceURL=webpack://semanticfinder/url_(ignored)?")
 /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
 /******/ 		var document = __webpack_require__.g.document;
 /******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
 /******/ 				scriptUrl = document.currentScript.src;
 /******/ 			if (!scriptUrl) {
 /******/ 				var scripts = document.getElementsByTagName("script");
